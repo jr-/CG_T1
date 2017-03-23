52d6ecf0585ca9478231f980a76b7d7d6396e52b
Andre de Carvalho <andre@Andres-MacBook-Pro.local>
2017-03-23 15:59:39 -0300

    Criação de matrizes de manipulação completa

--- Object.hpp
+++ Object.hpp
@@ -1,5 +1,6 @@
 #include <string>
 #include <vector>
+#include <math.h>
 using namespace std;
 
 class Coordinate {
@@ -15,6 +16,8 @@ class Coordinate {
           return _x;
         case 1:
           return _y;
+        case 2:
+          return 1.0;
         default:
           return 0.0;
       }}
@@ -36,9 +39,24 @@ class Object {
     string getName() const { return _name;}
     ObjectType getType() const {return _type;}
     vector<Coordinate>& getCoords() {return _coords;}
+    Coordinate getCenter();
     int getSize() const {return _coords.size();}
     void addCoordinate(double x, double y) {_coords.emplace_back(x,y);}
 };
+
+Coordinate Object::getCenter() {
+  int size = _coords.size();
+  double average_x, average_y;
+  for (int i = 0; i < size; i++){
+    average_x += _coords[i].getX();
+    average_y += _coords[i].getY();
+  }
+  average_x /= size;
+  average_y /= size;
+  Coordinate c = Coordinate(average_x,average_y);
+  return c;
+}
+
 //TODO usar enum??
 class Point: public Object {
   public:
@@ -57,15 +75,40 @@ class Polygon: public Object {
 
 namespace ObjectManipulation {
 
+enum RotationType { CENTER, ORIGIN, POINT };
+
 template <int rows, int columns>
 void translateMatrix(Coordinate vect, double translate_matrix[rows][columns]){
   for(int i = 0; i < rows; i++)
     for (int j= 0; j < columns; j++){
       if (i == j)
-        translate_matrix[i][j] = vect[j];
+        translate_matrix[i][j] = 1;
       else
         translate_matrix[i][j] = 0;
     }
+    for(int i = 0; i < columns; i++)
+      translate_matrix[rows-1][i] = vect[i];
+}
+
+template <int rows, int columns>
+void scaleMatrix(Coordinate factor, double scale_matrix[rows][columns]) {
+  for(int i = 0; i < rows; i++)
+    for (int j = 0; j < columns; j++){
+      if(i == j)
+        scale_matrix[i][j] = factor[j];
+      else
+        scale_matrix[i][j] = 0;
+    }
+}
+
+template <int rows, int columns>
+void rotateMatrix(int angle, double rotate_matrix[rows][columns]) {
+  for(int i = 0; i < rows; i++)
+    for(int j = 0; j<columns; j++)
+      rotate_matrix[i][j] = 0;
+  rotate_matrix[rows-1][columns-1] = 1;
+  rotate_matrix[0][0] = cos(angle); rotate_matrix[1][1] = rotate_matrix[0][0];
+  rotate_matrix[1][0] = sin(angle); rotate_matrix[0][1] = -rotate_matrix[1][0];
 }
 
 template <int r1, int c1, int r2, int c2>
@@ -90,7 +133,12 @@ void translateObject(Object* o, Coordinate vect) {
   }
 }
 
-void scaleObject(Object* o, double factor) {
+void scaleObject(Object* o, Coordinate factor) {
+  Coordinate center = o->getCenter();
+  
+}
+
+void rotateObject(Object* o,  double angle, RotationType rt, Coordinate reference) {
 
 }
 
